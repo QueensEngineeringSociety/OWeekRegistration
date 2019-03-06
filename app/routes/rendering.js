8a7bc0ba0c35bdbaff58ef13e20740ffc81ccd53
Carson Cook <cook.carson38@gmail.com>
2019-03-06 17:02:36 -0500

    abstracted render

--- app/routes/rendering.js
+++ app/routes/rendering.js
@@ -1,10 +1,31 @@
 const con = require("../../server/wufoo/wufooConstants");
-const views = require("../../server/util").views;
+const util = require("../../server/util");
 
-exports.render = function (result, view) {
-    result.render(view, {});
+const views = util.views;
+
+let renderObject = {
+    operators: con.operators,
+    headings: con.headings
+};
+
+exports.render = function (result, view, request, wufooData, groupNumbers, actionPath) {
+    result.render(view, buildRenderObject(request, wufooData, groupNumbers, actionPath));
 };
 
 exports.renderError = function (result, message) {
     result.render(views.ERROR, {errorMessage: message});
 };
+
+function buildRenderObject(request, wufooData, groupNumbers, actionPath) {
+    let isAdmin = util.isAdmin(request);
+    renderObject.fields = getAccessibleFields(isAdmin);
+    renderObject.isAdmin = isAdmin;
+    renderObject.wufoo = wufooData;
+    renderObject.groupNumbers = groupNumbers;
+    renderObject.actionPath = actionPath;
+    return renderObject;
+}
+
+function getAccessibleFields(isAdmin) {
+    return isAdmin ? con.allFields : con.generalFields;
+}
--- app/routes/search.js
+++ app/routes/search.js
@@ -4,6 +4,7 @@ const builder = require("../../server/wufoo/wufooQueryBuilder");
 const dbConn = require("../../config/database/queries.js");
 const util = require("../../server/util");
 const constants = require("../../server/util");
+const view = require("./rendering");
 
 const routes = constants.routes;
 const views = constants.views;
@@ -15,26 +16,13 @@ exports.get = {
 
 function getGeneral(request, result) {
     if (request.query['field'] && request.query['operator'] && request.query['value']) {
-        let accessFields = con.generalFields;
-        let admin = util.isAdmin(request);
-        if (admin) {
-            accessFields = con.allFields;
-        }
         wufoo.makeQuery(builder.customQuery(request.query['field'], request.query['operator'], request.query['value']), function (body) {
             dbConn.selectAll("groupData", function (err, rows) {
                 let groupNumbers = [];
                 for (let i in rows) {
                     groupNumbers[rows[i].wufooEntryId] = rows[i].groupNum; //i ID is unique
                 }
-                result.render(views.SEARCH, {
-                    wufoo: body,
-                    groupNumbers: groupNumbers,
-                    operators: con.operators,
-                    fields: accessFields,
-                    headings: con.headings,
-                    isAdmin: admin,
-                    actionPath: routes.SEARCH
-                });
+                view.render(result, views.SEARCH, request, body, groupNumbers, routes.SEARCH);
             });
         });
     }

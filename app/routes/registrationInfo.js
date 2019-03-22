bc7b0e382114ef3d0e0137313357c1082f6529dc
Carson Cook <cook.carson38@gmail.com>
2019-03-22 11:35:51 -0400

    cleaned regInfo

--- app/routes/registrationInfo.js
+++ app/routes/registrationInfo.js
@@ -22,155 +22,68 @@ exports.get = {
 };
 
 function getDisplayAll(request, result) {
-    let pageNum = request.query.nextPage ? parseInt(request.query.nextPage) : 0;
-    pageNum = request.query.prevPage ? parseInt(request.query.prevPage) : pageNum;
-    wufoo.makePaginatedQuery(pageNum, query.all, function (body, nextPageNum, prevPageNum) {
-        dbConn.selectAll("groupData", function (err, rows) {
-            let groupNumbers = [];
-            for (let i in rows) {
-                groupNumbers[rows[i].wufooEntryId] = rows[i].groupNum; //i ID is unique
-            }
-            view.renderPaginated(result, views.FILTER, request, body, groupNumbers, routes.FILTER, nextPageNum, prevPageNum);
-        });
-    });
+    getRequest(request, result, query.all, routes.FILTER);
 }
 
 function getDisplayAge(request, result) {
-    let pageNum = request.query.nextPage ? parseInt(request.query.nextPage) : 0;
-    pageNum = request.query.prevPage ? parseInt(request.query.prevPage) : pageNum;
-    wufoo.makePaginatedQuery(pageNum, query.age, function (body, nextPageNum, prevPageNum) {
-        dbConn.selectAll("groupData", function (err, rows) {
-            let groupNumbers = [];
-            for (let i in rows) {
-                groupNumbers[rows[i].wufooEntryId] = rows[i].groupNum; //i ID is unique
-            }
-            view.renderPaginated(result, views.FILTER, request, body, groupNumbers, routes.AGE, nextPageNum, prevPageNum);
-        });
-    });
+    getRequest(request, result, query.age, routes.AGE);
 }
 
 function getDisplayFood(request, result) {
-    let pageNum = request.query.nextPage ? parseInt(request.query.nextPage) : 0;
-    pageNum = request.query.prevPage ? parseInt(request.query.prevPage) : pageNum;
-    wufoo.makePaginatedQuery(pageNum, query.foodRestrictions, function (body, nextPageNum, prevPageNum) {
-        dbConn.selectAll("groupData", function (err, rows) {
-            let groupNumbers = [];
-            for (let i in rows) {
-                groupNumbers[rows[i].wufooEntryId] = rows[i].groupNum; //i ID is unique
-            }
-            view.renderPaginated(result, views.FILTER, request, body, groupNumbers, routes.FOOD_RESTRICTIONS, nextPageNum, prevPageNum);
-        });
-    });
+    getRequest(request, result, query.foodRestrictions, routes.FOOD_RESTRICTIONS);
 }
 
 function getDisplayPrimer(request, result) {
-    let pageNum = request.query.nextPage ? parseInt(request.query.nextPage) : 0;
-    pageNum = request.query.prevPage ? parseInt(request.query.prevPage) : pageNum;
-    wufoo.makePaginatedQuery(pageNum, query.wantPrimer, function (body, nextPageNum, prevPageNum) {
-        dbConn.selectAll("groupData", function (err, rows) {
-            let groupNumbers = [];
-            for (let i in rows) {
-                groupNumbers[rows[i].wufooEntryId] = rows[i].groupNum; //i ID is unique
-            }
-            view.renderPaginated(result, views.FILTER, request, body, groupNumbers, routes.PRIMER, nextPageNum, prevPageNum);
-        });
-    });
+    getRequest(request, result, query.wantPrimer, routes.PRIMER);
 }
 
 function getDisplayMedical(request, result) {
-    let pageNum = request.query.nextPage ? parseInt(request.query.nextPage) : 0;
-    pageNum = request.query.prevPage ? parseInt(request.query.prevPage) : pageNum;
-    wufoo.makePaginatedQuery(pageNum, query.medicalConcerns, function (body, nextPageNum, prevPageNum) {
-        dbConn.selectAll("groupData", function (err, rows) {
-            let groupNumbers = [];
-            for (let i in rows) {
-                groupNumbers[rows[i].wufooEntryId] = rows[i].groupNum; //i ID is unique
-            }
-            view.renderPaginated(result, views.FILTER, request, body, groupNumbers, routes.MEDICAL, nextPageNum, prevPageNum);
-        });
-    });
+    getRequest(request, result, query.medicalConcerns, routes.MEDICAL);
 }
 
 function getDisplayPronouns(request, result) {
-    let pageNum = request.query.nextPage ? parseInt(request.query.nextPage) : 0;
-    pageNum = request.query.prevPage ? parseInt(request.query.prevPage) : pageNum;
-    wufoo.makePaginatedQuery(pageNum, query.pronoun, function (body, nextPageNum, prevPageNum) {
-        dbConn.selectAll("groupData", function (err, rows) {
-            let groupNumbers = [];
-            for (let i in rows) {
-                groupNumbers[rows[i].wufooEntryId] = rows[i].groupNum; //i ID is unique
-            }
-            view.renderPaginated(result, views.FILTER, request, body, groupNumbers, routes.PRONOUNS, nextPageNum, prevPageNum);
-        });
-    });
+    getRequest(request, result, query.pronoun, routes.PRONOUNS);
 }
 
 function getDisplayAccessibility(request, result) {
-    let pageNum = request.query.nextPage ? parseInt(request.query.nextPage) : 0;
-    pageNum = request.query.prevPage ? parseInt(request.query.prevPage) : pageNum;
-    wufoo.makePaginatedQuery(pageNum, query.accessibilityConcerns, function (body, nextPageNum, prevPageNum) {
-        dbConn.selectAll("groupData", function (err, rows) {
-            let groupNumbers = [];
-            for (let i in rows) {
-                groupNumbers[rows[i].wufooEntryId] = rows[i].groupNum; //i ID is unique
-            }
-            view.renderPaginated(result, views.FILTER, request, body, groupNumbers, routes.ACCESSIBILITY, nextPageNum, prevPageNum);
-        });
-    });
+    getRequest(request, result, query.accessibilityConcerns, routes.ACCESSIBILITY);
 }
 
 function getDisplayPayPerson(request, result) {
-    let pageNum = request.query.nextPage ? parseInt(request.query.nextPage) : 0;
-    pageNum = request.query.prevPage ? parseInt(request.query.prevPage) : pageNum;
-    wufoo.makePaginatedQuery(pageNum, query.payInPerson, function (body, nextPageNum, prevPageNum) {
-        dbConn.selectAll("groupData", function (err, rows) {
-            let groupNumbers = [];
-            for (let i in rows) {
-                groupNumbers[rows[i].wufooEntryId] = rows[i].groupNum; //i ID is unique
-            }
-            view.renderPaginated(result, views.FILTER, request, body, groupNumbers, routes.PAY_PERSON, nextPageNum, prevPageNum);
-        });
-    });
+    getRequest(request, result, query.payInPerson, routes.PAY_PERSON);
 }
 
 function getDisplayPayMail(request, result) {
-    let pageNum = request.query.nextPage ? parseInt(request.query.nextPage) : 0;
-    pageNum = request.query.prevPage ? parseInt(request.query.prevPage) : pageNum;
-    wufoo.makePaginatedQuery(pageNum, query.payByMail, function (body, nextPageNum, prevPageNum) {
-        dbConn.selectAll("groupData", function (err, rows) {
-            let groupNumbers = [];
-            for (let i in rows) {
-                groupNumbers[rows[i].wufooEntryId] = rows[i].groupNum; //i ID is unique
-            }
-            view.renderPaginated(result, views.FILTER, request, body, groupNumbers, routes.PAY_MAIL, nextPageNum, prevPageNum);
-        });
-    });
+    getRequest(request, result, query.payByMail, routes.PAY_MAIL);
 }
 
 function getDisplayPayOnline(request, result) {
-    let pageNum = request.query.nextPage ? parseInt(request.query.nextPage) : 0;
-    pageNum = request.query.prevPage ? parseInt(request.query.prevPage) : pageNum;
-    wufoo.makePaginatedQuery(pageNum, query.payOnline, function (body, nextPageNum, prevPageNum) {
+    getRequest(request, result, query.payOnline, routes.PAY_ONLINE);
+}
+
+function getDisplayUnpaid(request, result) {
+    getRequest(request, result, query.unpaid, routes.UNPAID);
+}
+
+function getRequest(request, result, query, route){
+    let pageNum = getPageNum(request);
+    wufoo.makePaginatedQuery(pageNum, query, function (body, nextPageNum, prevPageNum) {
         dbConn.selectAll("groupData", function (err, rows) {
-            let groupNumbers = [];
-            for (let i in rows) {
-                groupNumbers[rows[i].wufooEntryId] = rows[i].groupNum; //i ID is unique
-            }
-            view.renderPaginated(result, views.FILTER, request, body, groupNumbers, routes.PAY_ONLINE, nextPageNum, prevPageNum);
+            let groupNumbers = getGroupNumbers(rows);
+            view.renderPaginated(result, views.FILTER, request, body, groupNumbers, route, nextPageNum, prevPageNum);
         });
     });
 }
 
-function getDisplayUnpaid(request, result) {
+function getGroupNumbers(rows) {
+    let groupNumbers = [];
+    for (let i in rows) {
+        groupNumbers[rows[i].wufooEntryId] = rows[i].groupNum; //i ID is unique
+    }
+    return groupNumbers;
+}
+
+function getPageNum(request){
     let pageNum = request.query.nextPage ? parseInt(request.query.nextPage) : 0;
-    pageNum = request.query.prevPage ? parseInt(request.query.prevPage) : pageNum;
-    wufoo.makePaginatedQuery(pageNum, query.unpaid, function (body, nextPageNum, prevPageNum) {
-        dbConn.selectAll("groupData", function (err, rows) {
-            let groupNumbers = [];
-            for (let i in rows) {
-                groupNumbers[rows[i].wufooEntryId] = rows[i].groupNum; //i ID is unique
-            }
-            view.renderPaginated(result, views.FILTER, request, body, groupNumbers, routes.UNPAID, nextPageNum, prevPageNum);
-        });
-    });
+    return request.query.prevPage ? parseInt(request.query.prevPage) : pageNum;
 }
